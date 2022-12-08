d3cb26ba1b4fefeb6eb58d9fd084948da14ce84b
FatimahAreola <fatimahyinka@gmail.com>
2022-12-08 14:02:12 -0600

    I think this is going to mess things up but

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
+import React from 'react';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useState } from 'react'
 import Cards from './Components/Cards'
-import Timer from './Components/Timer'
+import Login from "./Components/login";
+import EndGame from "./Components/EndGame";
+
 function App() {
-  const [state, setState]= useState(0)
-  return (
-    <div className="App">
-      <h1>Google Cloud Products</h1>
-      <Timer />
-      <Cards />
-    </div>
-  );
+    const [state, setState] = useState(0)
+    return (
+        <div className="ui container">
+            
+            <div className="App">
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/home" element={<Cards />}></Route>
+                        <Route path="/login" element={<Login />}></Route>
+                        <Route path="/EndGame" element={<EndGame />}></Route>
+                        </Routes>
+                </BrowserRouter>
+            </div>
+
+        </div>
+    );
 }
 
 export default App;
--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,86 +1,87 @@
 import { useState, useRef } from "react";
 import Card from './Card'
 import EndGame from "../EndGame";
+import Timer from './Components/Timer'
 
 
-export default function Cards(){
+export default function Cards() {
     const [cards, setCards] = useState([
-        {id:0, name: 'Bryan Canstron', status: '', img:'/images/01.jpg'},
-        {id:0, name: 'Bryan Canstron', status: '', img:'/images/01.jpg'},
-        {id:1, name: 'Bryan Canstron', status: '', img:'/images/02.jpg'},
-        {id:1, name: 'Bryan Canstron', status: '', img:'/images/02.jpg'},
-        {id:2, name: 'Bryan Canstron', status: '', img:'/images/03.png'},
-        {id:2, name: 'Bryan Canstron', status: '', img:'/images/03.png'},
-        {id:3, name: 'Bryan Canstron', status: '', img:'/images/04.jpg'},
-        {id:3, name: 'Bryan Canstron', status: '', img:'/images/04.jpg'},
-        {id:4, name: 'Bryan Canstron', status: '', img:'/images/05.jpg'},
-        {id:4, name: 'Bryan Canstron', status: '', img:'/images/05.jpg'},
-        {id:5, name: 'Bryan Canstron', status: '', img:'/images/06.jpg'},
-        {id:5, name: 'Bryan Canstron', status: '', img:'/images/06.jpg'},
-        {id:6, name: 'Bryan Canstron', status: '', img:'/images/07.jpg'},
-        {id:6, name: 'Bryan Canstron', status: '', img:'/images/07.jpg'},
-        {id:7, name: 'Bryan Canstron', status: '', img:'/images/08.jpg'},
-        {id:7, name: 'Bryan Canstron', status: '', img:'/images/08.jpg'},
-    ].sort(()=> Math.random()- 0.5))
+        { id: 0, name: 'Bryan Canstron', status: '', img: '/images/01.jpg' },
+        { id: 0, name: 'Bryan Canstron', status: '', img: '/images/01.jpg' },
+        { id: 1, name: 'Bryan Canstron', status: '', img: '/images/02.jpg' },
+        { id: 1, name: 'Bryan Canstron', status: '', img: '/images/02.jpg' },
+        { id: 2, name: 'Bryan Canstron', status: '', img: '/images/03.png' },
+        { id: 2, name: 'Bryan Canstron', status: '', img: '/images/03.png' },
+        { id: 3, name: 'Bryan Canstron', status: '', img: '/images/04.jpg' },
+        { id: 3, name: 'Bryan Canstron', status: '', img: '/images/04.jpg' },
+        { id: 4, name: 'Bryan Canstron', status: '', img: '/images/05.jpg' },
+        { id: 4, name: 'Bryan Canstron', status: '', img: '/images/05.jpg' },
+        { id: 5, name: 'Bryan Canstron', status: '', img: '/images/06.jpg' },
+        { id: 5, name: 'Bryan Canstron', status: '', img: '/images/06.jpg' },
+        { id: 6, name: 'Bryan Canstron', status: '', img: '/images/07.jpg' },
+        { id: 6, name: 'Bryan Canstron', status: '', img: '/images/07.jpg' },
+        { id: 7, name: 'Bryan Canstron', status: '', img: '/images/08.jpg' },
+        { id: 7, name: 'Bryan Canstron', status: '', img: '/images/08.jpg' },
+    ].sort(() => Math.random() - 0.5))
 
     const [previousCardState, setPreviousCardState] = useState(-1)
     const previousIndex = useRef(-1)
-    
+
     const [gameFinished, setGameFinished] = useState(false);
 
-    const matchCheck = (currentCard)=>{
-        if(cards[currentCard].id === cards[previousCardState].id ){
+    const matchCheck = (currentCard) => {
+        if (cards[currentCard].id === cards[previousCardState].id) {
             cards[previousCardState].status = 'active matched'
             cards[currentCard].status = 'active matched'
             setPreviousCardState(-1)
-            
 
-        }else{
+
+        } else {
             cards[currentCard].status = 'active'
             setCards([...cards])
-            setTimeout(()=>{
+            setTimeout(() => {
                 setPreviousCardState(-1)
                 cards[currentCard].status = 'unmatch'
-                cards[previousCardState].status='unmatch'
+                cards[previousCardState].status = 'unmatch'
                 setCards([...cards])
 
             }, 1000);
 
         }
         let matched = 0;
-        for(let i=0; i<16;i++){
-            if(cards[i].status === 'active matched')
+        for (let i = 0; i < 16; i++) {
+            if (cards[i].status === 'active matched')
                 matched = matched + 1
         }
-        if(matched === 16)
+        if (matched === 16)
             setGameFinished(true);
     }
 
 
-    const clickhandler = (index)=> {
-        if(index !== previousIndex.current){
-            if(cards[index].status === 'active matched'){
+    const clickhandler = (index) => {
+        if (index !== previousIndex.current) {
+            if (cards[index].status === 'active matched') {
                 alert('already matched')
             }
-            
-            
-            else{
 
 
-                if(previousCardState === -1){
+            else {
+
+
+                if (previousCardState === -1) {
                     previousIndex.current = index
                     cards[index].status = 'active'
                     setCards([...cards])
                     setPreviousCardState(index)
 
-                }else{
+                } else {
                     matchCheck(index)
-                    previousIndex.current=-1
+                    previousIndex.current = -1
 
                 }
             }
 
-        }else{
+        } else {
             alert('card currently selected')
 
         }
@@ -88,16 +89,21 @@ export default function Cards(){
 
 
     return (
-        <div className="container">
-            
-            {cards.map((card, index)=>{
-                return <Card card={card} key={index} index={index} clickhandler={clickhandler}/>
-            })}
-        {gameFinished && (
-        <EndGame
-          
-        />
-      )}
+        <div className="App">
+            <h1>Google Cloud Products</h1>
+            <Timer />
+
+            <div className="container">
+
+                {cards.map((card, index) => {
+                    return <Card card={card} key={index} index={index} clickhandler={clickhandler} />
+                })}
+                {gameFinished && (
+                    <EndGame
+
+                    />
+                )}
+            </div>
         </div>
     );
 }
