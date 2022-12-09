b6b842ecc8b3c12364c9c64d5d7b7896b4c4c576
FatimahAreola <fatimahyinka@gmail.com>
2022-12-08 18:16:08 -0600

    update landing page to route to home

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
         <div className="App">
             <BrowserRouter>
                 <Routes>
+                    <Route path="" element={<Home />}></Route>
                     <Route path="/home" element={<Home />}></Route>
                     <Route path="/login" element={<Login />}></Route>
                     <Route path="/EndGame" element={<EndGame />}></Route>
